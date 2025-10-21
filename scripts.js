499cec617309aa157ad979fa7d1da4a35ce2c811
Bartłomiej Budny <100838936+budbar@users.noreply.github.com>
2025-10-21 15:02:20 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,18 +1,41 @@
-
-
-window.addEventListener("DOMContentLoaded", () => {
+function getLocation() {
     if(navigator.geolocation) {
         let latitude = document.getElementById("latitude");
         let longitude = document.getElementById("longitude");
 
         navigator.geolocation.getCurrentPosition((position) => {
-            console.log("Szerokość geograficzna: " + position.coords.latitude);
-            console.log("Długość geograficzna: " + position.coords.longitude)
-
             latitude.innerHTML = "Szerokość geograficzna: " + position.coords.latitude;
             longitude.innerHTML = "Długość geograficzna: " + position.coords.longitude;
         })
     } else {
         console.log("Geolokalizacja nie działa!");
     }
+}
+
+function getAxis() {
+    if ('Gyroscope' in window) {
+        try {
+            let gyroscope = new Gyroscope({ frequency: 60 });
+            let x = document.getElementById("x");
+            let y = document.getElementById("y");
+            let z = document.getElementById("z");
+
+
+            gyroscope.addEventListener("reading", () => {
+                x.innerHTML = "Współrzędna X: " + gyroscope.x
+                y.innerHTML = "Współrzędna Y: " + gyroscope.y
+                z.innerHTML = "Współrzędna Z: " + gyroscope.z
+            });
+
+            gyroscope.start();
+        } catch (error) {
+            console.error("Błąd przy uruchamianiu żyroskopu:", error);
+        }
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    getLocation();
+    getAxis();
 });
+
